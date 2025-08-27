9e2793c1848fbf921ca7760dc178175704bb39b3
Yasmin028 <ylvelasco12@ucatolica.edu.co>
2025-08-26 20:49:43 -0500

    Geometric 27

--- src/geometric/geometric.py
+++ src/geometric/geometric.py
@@ -318,13 +318,18 @@ class Geometria:
         Returns:
             tuple: Coeficientes (A, B, C) de la ecuación de la recta
         """
-        if x1 == x2 and y1 == y2:
-            raise ValueError("Los puntos no pueden ser iguales, no definen una recta.")
-        A = y1 - y2
-        B = x2 - x1
-        C = x1 * y2 - x2 * y1
-        if A < 0 or (A == 0 and B < 0):
-            A, B, C = -A, -B, -C
+        A = y2 - y1
+        B = x1 - x2
+        C = x2*y1 - x1*y2
+
+        if A == 0 and B > 0:  
+            B = -B
+            C = -C
+        elif A < 0:  
+            A = -A
+            B = -B
+            C = -C
+
         return (A, B, C)
     
     def area_poligono_regular(self, num_lados, lado, apotema):
